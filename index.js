4a8826e200119c633c8b1c0132c0dba167758838
Elbehary <ahmedelbehary0@gmail.com>
2018-05-29 10:42:36 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,60 @@
+require("dotenv").config();
+var uuidv4 = require("uuid/v4");
+var express = require("express");
+var app = express();
+var fs = require("fs");
+
+app.set("views", "./views");
+app.use(express.static("public"));
+app.set("view engine", "pug");
+
+app.get("/", function(req, res) {
+  res.render("index", { title: "Hey", message: "Hello there!" });
+});
+
+app.get("/user/create", (req, res) => {
+  var id = uuidv4();
+  var obj = {
+    uuid: id, //every user should become a uuid. https://www.npmjs.com/package/uuid
+    email: req.query.email, //from form
+    pw: req.query.pw, //from form
+    status: "unconfirmed", //default value unconfirmed, every register is first unconfirmed
+    session: true //depending on the RememberMe checkbox
+  };
+
+  fs.writeFile(`./users/${id}.json`, JSON.stringify(obj), function(err) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log("ths file was saved");
+    res.redirect("/");
+    console.log(req.query);
+  });
+});
+
+app.get("/user/verify/:token", (req, res) => {
+  console.log(req.params.token);
+  //open your files folder fs.readDir
+  fs.readFile(`./users/${req.params.token}.json`, (err, file) => {
+    if (err) console.log("Error", err);
+    else {
+      var file = JSON.parse(file);
+      //set that document status to confirmed
+      if (file.uuid === req.params.token) {
+        console.log("current file uuid:", file.uuid);
+        file.status = "confirmed";
+        //write file back
+        fs.writeFile(`./users/${req.params.token}.json`, JSON.stringify(file), err => {
+          if (err) {
+            return console.log(err);
+          }
+
+          console.log("The Email was confirmed!");
+          //redirect to /
+          res.redirect("/");
+        });
+      }
+    }
+  });
+});
+app.listen(4000, () => console.log("Example app listening on port 4000!"));
